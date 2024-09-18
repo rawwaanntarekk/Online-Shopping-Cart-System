a81dd04565fb409ac241e6e1e3933a0aa04376f2
Rawan Tarek <115898539+rawwaanntarekk@users.noreply.github.com>
2024-09-18 11:33:29 +0300

    Add category enum to product, add main attributes to product model

--- src/DAL/Models/Product.java
+++ src/DAL/Models/Product.java
@@ -1,4 +1,21 @@
 package DAL.Models;
 
 public class Product {
+    static int id = 0;
+    String name;
+    String description;
+    double price;
+    int quantityInStock;
+    Category category;
+
+    public Product(String name, String description, double price, int quantityInStock, Category category) {
+        id++;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.quantityInStock = quantityInStock;
+        this.category = category;
+    }
+
+
 }
