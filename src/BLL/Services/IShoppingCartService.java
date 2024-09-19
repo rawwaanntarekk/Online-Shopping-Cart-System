0ad861f59f8d7748e133c8aabc10c9fd77cd38e8
hudaamr <94346307+hudaamr@users.noreply.github.com>
2024-09-19 10:48:25 +0300

    Update IShoppingCartService.java

--- src/BLL/Services/IShoppingCartService.java
+++ src/BLL/Services/IShoppingCartService.java
@@ -1,4 +1,11 @@
 package BLL.Services;
 
+import java.util.ArrayList;
+
 public interface IShoppingCartService {
+    void addProduct(Product product, int quantity);
+    void removeProduct(int productId);
+    ArrayList<CartItem> getProducts();
+    void clearCart();
+    double calculateTotal();
 }
